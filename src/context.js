815874f09703cb3f070b6e66f660db5013be0a5d
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-13 17:27:35 +0530

    add authStatus context

--- src/context.js
+++ src/context.js
@@ -1,23 +1,26 @@
-import React from "react";
-import { useContext, useEffect, useState } from "react";
+import React from 'react';
+import { useContext, useEffect, useState } from 'react';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [authStatus, setAuthStatus] = useState(false);
 
   useEffect(() => {
     if (!isMenuOpen) {
-      document.getElementsByTagName("body")[0].style.overflow = "scroll";
+      document.getElementsByTagName('body')[0].style.overflow = 'scroll';
     } else {
-      document.getElementsByTagName("body")[0].style.overflow = "hidden";
+      document.getElementsByTagName('body')[0].style.overflow = 'hidden';
     }
   }, [isMenuOpen]);
-  
+
   return (
     <AppContext.Provider
       value={{
         isMenuOpen,
         setIsMenuOpen: setIsMenuOpen,
+        authStatus,
+        setAuthStatus: setAuthStatus,
       }}
     >
       {children}
